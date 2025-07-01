87e8e0301a1a67fdea74c1d412559d6552775230
zhaoweijie <zhaoweijie1108@qq.com>
2025-07-01 16:50:51 +0800

    添加日志

--- PancakeSwap.Api/Program.cs
+++ PancakeSwap.Api/Program.cs
@@ -1,12 +1,15 @@
+using PancakeSwap.Application.Database.Config;
 using PancakeSwap.Infrastructure.Database;
 using PancakeSwap.Infrastructure.Database.Migrations;
+using QYQ.Base.Common.IOCExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.AddQYQSerilog();
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<PancakeSwap.Infrastructure.Database.ApplicationDbContext>();
+builder.Services.AddSingleton<ApplicationDbContext>();
+builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection("ConnectionStrings:Default"));
 
 var app = builder.Build();
 
